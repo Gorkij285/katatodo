38f7168e3fdd6ca93f7de17c187b07963a8802d3
Gorkij285 <gorkij285@gmail.com>
2023-04-17 19:47:07 +0300

    gg

--- src/Task/Task.jsx
+++ src/Task/Task.jsx
@@ -12,13 +12,50 @@ const Span = (prop) => {
 class Task extends React.Component {
   state = {
     dt: Date.now(),
+    time: 0,
+    intervalId: null,
   }
   static propTypes = {
     value: PropTypes.string,
   }
 
+  handleClick = () => {
+    const intervalId = setInterval(() => {
+      this.setState(prevState => ({
+        time: prevState.time + 1
+      }));
+    }, 1000);
+    this.setState({ intervalId });
+  };
+
+  handleStart = () => {
+    console.log('СТАРТ')
+    if (this.state.intervalId) return;
+    const intervalId = setInterval(() => {
+      this.setState(prevState => ({
+        time: prevState.time + 1
+      }));
+    }, 1000);
+    this.setState({ intervalId });
+  };
+
+  handlePause = () => {
+    console.log('СТОП')
+    if (this.state.intervalId) {
+      clearInterval(this.state.intervalId);
+      this.setState({ intervalId: null });
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
+
   render() {
+    console.log(this.props)
     const { value, toggleDone, toggleCompleted, done } = this.props
+    const {time} = this.state
+
     let classNames = ''
 
     if (!done) {
@@ -26,6 +63,11 @@ class Task extends React.Component {
     }
 
     let result = formatDistanceToNow(new Date(this.state.dt), { includeSeconds: true }, { addSuffix: true })
+
+    let watch = time ?
+                <span className='timer'>{formatTime(time)}</span>:
+                <span className='timer'>{'00:00:00'}</span>
+
 
     return (
       <li className={classNames}>
@@ -35,12 +77,29 @@ class Task extends React.Component {
             <Span value={value} />
             <span className="created">{result}</span>
           </label>
+          <div>
+            {watch}
+            <button className="timerButOne"onClick={this.handleStart} >
+              Start
+            </button>
+            <button className="timerButTwo" onClick={this.handlePause} >
+              Pause
+            </button>
+          </div>
           <button className="icon icon-edit"></button>
           <button className="icon icon-destroy" onClick={toggleCompleted}></button>
         </div>
       </li>
     )
   }
+}
+function formatTime(seconds) {
+  const hours = Math.floor(seconds / 3600);
+  seconds = seconds % 3600;
+  const minutes = Math.floor(seconds / 60);
+  seconds = seconds % 60;
+  const formattedTime = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+  return formattedTime;
 }
 
 Task.defaultProps = {
---
+++ src/TaskTimer/TaskTimer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './TaskList.css'
+
+export default TaskTimer = () => {
+
+    const handleStart = () => {
+
+    }
+
+    const handlePause = () => {
+        
+    }
+
+
+    return (
+        <div>
+            <div>{formatTime(timer)}</div>
+            <button onClick={handleStart}>Start</button>
+            <button onClick={handlePause}>Pause</button>
+        </div>
+    );
+}
